0f23cde85e08361e92f8028b5c059538f1a7b330
Ahmed Khatib <ahmedkhatib99@gmail.com>
2024-11-26 13:59:02 +0530

    Added Message and Room tables in the database.

--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
+from . import models
+
 
 # Create your views here.
 
-rooms = [
-    {"id": 1, "name": "Python for beginners."},
-    {"id": 2, "name": "Learn FastAPI with me."},
-    {"id": 3, "name": "Diving in Django together."},
-]
+# rooms = [
+#     {"id": 1, "name": "Python for beginners."},
+#     {"id": 2, "name": "Learn FastAPI with me."},
+#     {"id": 3, "name": "Diving in Django together."},
+# ]
 
 def home(request):
+    rooms = models.Room.objects.all()
     context = {"rooms": rooms}
     return render(request, "base/home.html", context)
 
 def room(request, pk):
-    context = {"pk": pk}
+    room = models.Room.objects.get(id = pk)
+    context = {"room": room}
     return render(request, "base/room.html", context)
